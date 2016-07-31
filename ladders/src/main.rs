1e97ff0d7230687223db5d356b8e515923c67e57
Jimmy Börjesson <alcesleo@gmail.com>
2016-07-31 15:57:16 +0200

    Clean up logic for filtering long words

--- ladders/src/main.rs
+++ ladders/src/main.rs
@@ -5,24 +5,22 @@ mod graph;
 use graph::Graph;
 
 fn main() {
-    let dict = dictionary();
-    let num_letters = 5;
+    let max_letters = 5;
+    let words = dictionary(max_letters);
 
-    let words: Vec<_> = dict.iter()
-                            .filter(|word| word.len() == num_letters)
-                            .collect();
-
-    println!("{}", words.len());
-
-    let graph = Graph::construct(words);
+    let graph = Graph::construct(&words);
     graph.display();
 }
 
-fn dictionary() -> Vec<String> {
+fn dictionary(length_limit: usize) -> Vec<String> {
     let mut file   = File::open("/usr/share/dict/words").unwrap();
     let mut result = String::new();
 
     file.read_to_string(&mut result);
 
-    result.split("\n").map(str::to_string).collect()
+    result
+        .split("\n")
+        .map(str::to_string)
+        .filter(|word| word.len() == length_limit)
+        .collect()
 }
